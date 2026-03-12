7b0864ce8bad3d1b58d92973bbf06b54c8371306
Akari_VT <astilcodex6996@gmail.com>
2026-03-12 17:08:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const urlParams = new URLSearchParams(queryString);
   const contentURL = urlParams.get('url');
-document.getElementById('frame').src = contentURL
+document.getElementById('frame').src = contentURL.slice(1)
