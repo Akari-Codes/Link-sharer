e76a9a3ce47646d0c18e091e8e95b08443d1b32b
Akari_VT <astilcodex6996@gmail.com>
2026-03-12 18:31:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const contentURL = urlParams.get('url');
 document.getElementById('frame').src = contentURL;
-const title = document.getElementById('frame').document.title
-document.getElementById('title').innerText = title
+const title = document.getElementById('frame').document.title;
+document.getElementById('title').innerText = title;
