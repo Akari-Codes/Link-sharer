672c181834d29d3fbaf64d51f69074de8401ad6b
Akari_VT <astilcodex6996@gmail.com>
2026-03-12 17:06:27 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var currentURL = window.location.href;
-var index = currentURL.indexOf('=')
-var contentURL = currentURL.slice(index)
+const urlParams = new URLSearchParams(queryString);
+  const contentURL = urlParams.get('url');
 document.getElementById('frame').src = contentURL
