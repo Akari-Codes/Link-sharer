cc0e2eeeeaf464f6f85a807c636498f7f17af68c
Akari_VT <astilcodex6996@gmail.com>
2026-03-12 18:31:30 +0000

    Update share.js

--- share.js
+++ share.js
@@ -1,11 +1,8 @@
 function new_share() {
-  var url = document.getElementById('share').value
-  document.getElementById('preview_text').innerText = "Preview Content"
-  document.getElementById('preview').style.display = "inline-block"
-  document.getElementById('preview').src = url
-  var share = "https://akari-codes.github.io/Link-sharer/index.html?url=" + url
-  document.getElementById('share_url').innerText = "Your Share URL - " + share
+  var url = document.getElementById('share').value;
+  document.getElementById('preview_text').innerText = "Preview Content";
+  document.getElementById('preview').style.display = "inline-block";
+  document.getElementById('preview').src = url;
+  var share = "https://akari-codes.github.io/Link-sharer/index.html?url=" + url;
+  document.getElementById('share_url').innerText = "Your Share URL - " + share;
 }
-
-
-
