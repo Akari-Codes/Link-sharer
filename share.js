6a78afb1a96c3ad5d67c43c6160cc6e17948f028
Akari_VT <astilcodex6996@gmail.com>
2026-03-20 15:27:08 +0000

    s

--- share.js
+++ share.js
@@ -1,18 +1,22 @@
-function new_share() {
-  var url = document.getElementById('share').value;
-  var mode = document.getElementById('share_type').value;
-  if(mode == "direct-link") {
-    format_share(url);
-  } else if(mode == "google-drive-share") {
-    url.replace('file/d/','uc?export=download&id=');
-    url.replace('/view?usp=sharing','');
-    format_share(url);
-  };
-};
 function format_share(url) {
 document.getElementById('preview_text').innerText = "Preview Content";
 document.getElementById('preview').style.display = "inline-block";
 document.getElementById('preview').src = url;
 var share = "https://akari-codes.github.io/Link-sharer/index.html?url=" + url;
 document.getElementById('share_url').innerText = "Your Share URL - " + share;
+};
+function gdrive() {
+  var url = document.getElementById('share').value;
+  url.replace('file/d/','uc?export=download&id=');
+  url.replace('/view?usp=sharing','');
+  format_share(url);
+};
+function directlink() {
+var url = document.getElementById('share').value;
+format_share(url);
+};
+function reload() {
+var type = document.getElementById('share_type').value;
+var func = type + "()";
+document.getElementById('make_share').onchange = func;
 };
